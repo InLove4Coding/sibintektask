81ddd5fdc954bce0cae9acd8321e5ebf91cf5f4e
Evgeniy Kuzmin <evgeneprog@gmail.com>
2020-06-10 18:32:32 +0400

    Initial commit

---
+++ src/main/java/com/kuzmin/evgenii/sibintek/SibintekApplication.java
@@ -0,0 +1,13 @@
+package com.kuzmin.evgenii.sibintek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SibintekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SibintekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kuzmin/evgenii/sibintek/SibintekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kuzmin.evgenii.sibintek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SibintekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
