18afc226fc0da361f15cf67f658b7f455463e677
Evgeniy Kuzmin <evgenii.bad@gmail.com>
2020-07-06 17:15:30 +0300

    Переделал работу под Postgres в связи с большим удобством для меня

--- src/main/java/com/kuzmin/evgenii/sibintek/model/Worker.java
+++ src/main/java/com/kuzmin/evgenii/sibintek/model/Worker.java
@@ -19,12 +19,12 @@ public class Worker {
     //Имя рабочего
     private String name;
     //Отдел, где работает работник
-    @JoinColumn(name = "department_id",foreignKey =@ForeignKey(name = "WORKER_TO_DEPARMENT"))
+    @JoinColumn(name = "department_id",foreignKey =@ForeignKey(name = "worker_to_department"))
     @ManyToOne(fetch = FetchType.LAZY)
     private Department department;
     //Список его смен
 
-    //mappeedBy Названия поля в workshift
+    //mapped By Названия поля в workshift
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "worker")
     private Set<WorkShift> workShiftList;
 
